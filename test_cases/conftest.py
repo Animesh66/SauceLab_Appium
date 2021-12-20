d32db0ba29e831004d604ab1c92afd0f2e8006c4
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-12-20 17:52:49 +0530

    conftest file modified for SauceLab

--- test_cases/conftest.py
+++ test_cases/conftest.py
@@ -22,7 +22,8 @@ def appium_driver(request):
         'name': request.node.name,
         'appiumVersion': '1.20.2',
         'capturePerformance': True,
-        'extendedDebugging': True
+        'extendedDebugging': True,
+        'browserName' : 'Chrome'
     }
 
     desired_caps = {}
@@ -31,9 +32,9 @@ def appium_driver(request):
     # desired_caps['appium:deviceName'] = 'Android GoogleAPI Emulator'
     # desired_caps['appium:app'] = 'storage:filename=Simple_Demo.apk'
     # desired_caps['appium:app']='https://github.com/Animesh66/SauceLab_Appium/blob/95a35791314a9d0f53667ca945ba8091930beb43/Simple_Demo.apk'
+    desired_caps['appium:deviceName'] = 'Android GoogleAPI Emulator'
     desired_caps['appium:platformName'] = 'Android'
     desired_caps['appium:platformVersion'] = '10.0'
-    desired_caps['appium:deviceName'] = 'Android GoogleAPI Emulator'
     desired_caps['appium:app'] = 'storage:filename=mda-1.0.8-10.apk'
     desired_caps['sauce:options'] = sauce_options
     driver = webdriver.Remote(sauce_url, desired_caps)
